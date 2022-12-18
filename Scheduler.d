c46a9025bfde845bd999db589b21e3ea342eec5c
danielkaboto <danielknspdk@gmail.com>
2022-12-19 00:08:59 +0200

    initial commit

---
+++ Scheduler.d
@@ -0,0 +1,1 @@
+obj/Scheduler.o: src/Scheduler.cpp
---
+++ src/Scheduler.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int choice;
+    do
+    {
+        cout<< "-------------------- CPU SCHEDULING SIMULATOR-------------------------------"<< endl;
+        cout<<" 1. SCHEDULING METHOD(NONE)\n 2. PREEMPTIVE MODE(OFF)\n 3. SHOW RESULT\n 4.END PROGRAM\n";
+        cout<<" Option > ";
+        cin >> choice;
+        switch (choice)
+        {
+            case 1:
+                /* code */
+                break;
+            case 2:
+                /* code */
+                break;
+            case 3:
+                /* code */
+                break;
+            case 4:
+                return 0;
+                break;
+            
+            default:
+                cout <<"please enter the correct choice !!" << endl;
+                break;
+        }
+
+    }
+    while(choice>0 && choice <=4);
+
+    return 0;
+
+
+
+}
