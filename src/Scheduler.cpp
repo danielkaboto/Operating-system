47406ceabe67460df9c25776597e2c77a8e334e5
danielkaboto <danielknspdk@gmail.com>
2022-12-20 13:19:17 +0200

    working on main funtion(take argument)

--- src/Scheduler.cpp
+++ src/Scheduler.cpp
@@ -1,14 +1,55 @@
+//importing the libraries needed for this project
+
 #include <iostream>
+#include <unistd.h>
+#include <fstream>
 using namespace std;
 
+// prototypes of all the define function 
 void Scheduling_Method();
 void Preemptive_Mode();
 void Show_Result();
 
 
 
-int main(void)
+int main(int argc , char *argv[])
 {
+    // reading the argument pass on the command line
+    int counter;
+    string File_in;      //store the input file
+    string File_out;    // store the ouput file
+
+    while((counter =getopt(argc,argv, "i:o:")) != -1)
+    {
+        switch (counter)
+        {
+        case 'i':
+            File_in = optarg;   //assign the value of the optarg(content the file.txt used as input in the code) to File_in
+            break;
+        case 'o':
+            File_out = optarg;   //assign the value of the optarg(content the file.txt used as output the code) to File_out
+            break;
+        
+        case '?':
+            break;
+        }
+    }
+// for debuging process checking if the input file open perfectly wihout problem
+    ifstream infile(File_in);
+    if(!infile.is_open())
+    {
+        cout<<"Error: the input file cannot be open !!" <<endl;
+        return 1;
+    }
+
+// Open the output file
+    ofstream outfile(File_out);
+    if(!outfile.is_open())
+    {
+        cout <<"Error : the output file cannot be open !!"<<endl;
+        return 1;
+    }
+
     int choice;
     do
     {
@@ -38,6 +79,15 @@ int main(void)
 
     }
     while(choice>0 && choice <=4);
+
+
+    // Read the input file and Write to the output file
+
+    string Line;    // keep the string value came form the input values
+    while(getline(infile,Line))
+    {
+      outfile << Line;
+    }
 
 
 
